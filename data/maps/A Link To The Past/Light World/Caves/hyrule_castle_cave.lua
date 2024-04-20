0c74f3cab1ffa8c44368074ec992ee1a56ef2297
ZeldoRetro <shadar669@gmail.com>
2024-04-20 00:21:52 -0400

    Ajout map Hyrule Castle Cave

--- data/maps/A Link To The Past/Light World/Caves/hyrule_castle_cave.lua
+++ data/maps/A Link To The Past/Light World/Caves/hyrule_castle_cave.lua
@@ -1,24 +1,37 @@
--- Lua script of map A Link To The Past/Light World/Caves/lost_wood_cave - Copie (13).
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+local door_manager = require("scripts/maps/door_manager")
+door_manager:manage_map(map)
+local chest_manager = require("scripts/maps/chest_manager")
+chest_manager:manage_map(map)
+local separator_manager = require("scripts/maps/separator_manager")
+separator_manager:manage_map(map)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+--GESTION DE LUMIERE DANS LA PIECE ET NIVEAUX D'OBSCURITE
+require("scripts/maps/light_manager.lua")
+local dark = sol.surface.create(320,240)
+dark:set_opacity(50)
+dark:fill_color({0, 0, 0})
+local torch_on = 0
+for torch in map:get_entities("timed_torch_") do
+  function torch:on_lit()
+    torch_on = torch_on + 1
+    local opacity = dark:get_opacity()
+    dark:set_opacity(math.max(opacity - 50,0))
+    sol.timer.start(10000,function()
+      torch_on = torch_on - 1
+      if torch_on <= 3 then
+        local opacity2 = dark:get_opacity()
+        dark:set_opacity(math.min(opacity2 + 50,255)) 
+      end
+    end)
+  end
 end
+map:register_event("on_draw", function(map, dst_surface)
+  dark:draw(dst_surface)
+end)
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
+function map:on_started()
+  
 end
