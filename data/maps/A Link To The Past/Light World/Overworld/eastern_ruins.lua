37ad8f366d44ee74ed19a784eedb84a027d6018b
KaKaShUruKioRa <69737302+KaKaShUruKioRa@users.noreply.github.com>
2024-06-20 05:47:15 +0200

    Dossiers A Link to the Past v0.3.0 Public Version

--- data/maps/A Link To The Past/Light World/Overworld/eastern_ruins.lua
+++ data/maps/A Link To The Past/Light World/Overworld/eastern_ruins.lua
@@ -11,10 +11,18 @@ local map = ...
 local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
+function map:on_started(destination)
   -- You can initialize the movement and sprites of various
-  -- map entities here.
+  -- map entities here.  
+  if destination == dest_dungeon_eastern_ruins_0 then
+      if game:get_value("demo_part_2_dialog_ok") then return end
+      if game:get_value("get_pendant_of_courage") then
+      local dialog_box = game:get_dialog_box()
+        dialog_box:set_style("empty")
+        game:start_dialog("demo.part_two",function() dialog_box:set_style("box") game:set_value("demo_part_2_dialog_ok",true) end)
+  end
+end
+
 end
 
 -- Event called after the opening transition effect of the map,
--- data/maps/A Link To The Past/Light World/Overworld/sanctuary.lua
+++ data/maps/A Link To The Past/Light World/Overworld/sanctuary.lua
@@ -9,16 +9,26 @@
 
 local map = ...
 local game = map:get_game()
-
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
 
+function map:on_started()
   -- You can initialize the movement and sprites of various
   -- map entities here.
+  if game:get_value("demo_part_1_dialog_ok") then
+    sensor_demo_part_one_ended:set_enabled(false)
+  end
 end
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
 function map:on_opening_transition_finished()
 
+end
+
+-- Event : Démo Partie 1 Terminée
+function sensor_demo_part_one_ended:on_activated()
+  self:set_enabled(false)
+      local dialog_box = game:get_dialog_box()
+      dialog_box:set_style("empty")
+      game:start_dialog("demo.part_one",function() dialog_box:set_style("box") game:set_value("demo_part_1_dialog_ok",true) end)
 end
