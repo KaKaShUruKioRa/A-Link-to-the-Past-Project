590e46f312a23a3c4e24871d8390e97fe68461b6
ZeldoRetro <shadar669@gmail.com>
2025-03-05 21:47:53 -0500

    Mapping Cavernes Death Mountain East

--- data/maps/A Link To The Past/Light World/Caves/Death Mountain East/F1_spiral_cave.lua
+++ data/maps/A Link To The Past/Light World/Caves/Death Mountain East/F1_spiral_cave.lua
@@ -1,24 +1,20 @@
--- Lua script of map A Link To The Past/Light World/Caves/lost_wood_cave - Copie (11).
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
 
--- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+local door_manager = require("scripts/maps/door_manager")
+door_manager:manage_map(map)
+local chest_manager = require("scripts/maps/chest_manager")
+chest_manager:manage_map(map)
+local separator_manager = require("scripts/maps/separator_manager")
+separator_manager:manage_map(map)
 
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
-end
+map:register_event("on_started",function(map, destination)
 
--- Event called after the opening transition effect of the map,
--- that is, when the player takes control of the hero.
-function map:on_opening_transition_finished()
-
-end
+  local ground=game:get_value("tp_ground")
+  if ground=="hole" then
+    hero:set_visible(false)
+  else
+    hero:set_visible()
+  end
+  
+end)
